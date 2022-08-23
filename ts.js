058cd3d7f8bce62c0a7b5bc35d9cc0ed2b26e599
saiashish9 <saiashish3760@gmail.com>
2022-08-24 00:52:36 +0530

    k

--- ts.js
+++ ts.js
@@ -11,8 +11,25 @@ binaryTree.left.right = new TreeNode(4);
 binaryTree.right = new TreeNode(5);
 binaryTree.right.right = new TreeNode(6);
 
-function dfsH() {}
+function dfsH(head, result, curr = [], currLength = 0) {
+  if (head) {
+    curr[currLength] = head.data;
+    currLength++;
+    if (!head.left && !head.right) {
+      currLength = 0;
+      result.push(curr.slice());
+      return;
+    }
+    dfsH(head.left, result, curr, currLength);
+    dfsH(head.right, result, curr, currLength);
+  }
+}
 
-function dfs() {}
+function dfs() {
+  const result = [];
+  dfsH(binaryTree, result);
+  console.log(result);
+}
 
 dfs(binaryTree);
+// [ [ 1, 2, 3 ], [ 1, 2, 4 ], [ 1, 5, 6 ] ]
