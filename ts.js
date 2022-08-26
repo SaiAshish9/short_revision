21b15cab4c9f946b6e81588db50a66c3e2607fbd
saiashish9 <saiashish3760@gmail.com>
2022-08-26 08:32:46 +0530

    k

--- ts.js
+++ ts.js
@@ -11,27 +11,27 @@ binaryTree.left.right = new TreeNode(4);
 binaryTree.right = new TreeNode(5);
 binaryTree.right.right = new TreeNode(6);
 
-function dfsH(head, result, curr = [], currLength = 0) {
-  if (head) {
-    curr[currLength] = head.data;
-    currLength++;
-    if (!head.left && !head.right) {
-      currLength = 0;
-      result.push(curr.slice());
-      return;
-    }
-    dfsH(head.left, result, curr, currLength);
-    dfsH(head.right, result, curr, currLength);
-  }
-}
+// function dfsH(head, result, curr = [], currLength = 0) {
+//   if (head) {
+//     curr[currLength] = head.data;
+//     currLength++;
+//     if (!head.left && !head.right) {
+//       currLength = 0;
+//       result.push(curr.slice());
+//       return;
+//     }
+//     dfsH(head.left, result, curr, currLength);
+//     dfsH(head.right, result, curr, currLength);
+//   }
+// }
 
-function dfs() {
-  const result = [];
-  dfsH(binaryTree, result);
-  console.log(result);
-}
+// function dfs() {
+//   const result = [];
+//   dfsH(binaryTree, result);
+//   console.log(result);
+// }
 
-dfs(binaryTree);
+// dfs(binaryTree);
 // [ [ 1, 2, 3 ], [ 1, 2, 4 ], [ 1, 5, 6 ] ]
 
 
