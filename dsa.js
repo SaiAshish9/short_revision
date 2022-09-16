23b158fe1696289545d910a1036d87988de0a219
saiashish9 <saiashish3760@gmail.com>
2022-09-16 23:14:13 +0530

    k

--- dsa.js
+++ dsa.js
@@ -1509,40 +1509,38 @@
 // //   = [ [ 4, 3 ], [ 5, 3 ], [ 5, 6 ], [ 1, 2 ] ];
 // // console.log(maxEnvelopes(envelopes))
 
-var isMatch = function(s, p) {
+var isMatch = function (s, p) {
   const rows = s.length;
   const columns = p.length;
   if (rows == 0 && columns == 0) {
-      return true;
+    return true;
   }
   if (columns == 0) {
-      return false;
+    return false;
   }
-  const dp = Array.from({
-      length: s.length + 1
-  }, () => [false]);
+  const dp = Array.from(Array(rows + 1), () => Array(columns + 1).fill(false));
   dp[0][0] = true;
   for (let i = 1; i < columns + 1; i++) {
-      if (p[i - 1] === "*") {
-          dp[0][i] = dp[0][i - 2];
-      } else {
-          dp[0][i] = false;
-      }
+    if (p[i - 1] === "*") {
+      dp[0][i] = dp[0][i - 2];
+    } else {
+      dp[0][i] = false;
+    }
   }
   for (let i = 1; i < rows + 1; i++) {
-      for (let j = 1; j < columns + 1; j++) {
-          if (p[j - 1] === "*") {
-              if (p[j - 2] === s[i - 1] || p[j - 2] === ".") {
-                  dp[i][j] = dp[i][j - 2] || dp[i - 1][j];
-              } else {
-                  dp[i][j] = dp[i][j - 2];
-              }
-          } else if (p[j - 1] === s[i - 1] || p[j - 1] === ".") {
-              dp[i][j] = dp[i - 1][j - 1];
-          } else {
-              dp[i][j] = false;
-          }
+    for (let j = 1; j < columns + 1; j++) {
+      if (p[j - 1] === "*") {
+        if (p[j - 2] === s[i - 1] || p[j - 2] === ".") {
+          dp[i][j] = dp[i][j - 2] || dp[i - 1][j];
+        } else {
+          dp[i][j] = dp[i][j - 2];
+        }
+      } else if (p[j - 1] === s[i - 1] || p[j - 1] === ".") {
+        dp[i][j] = dp[i - 1][j - 1];
+      } else {
+        dp[i][j] = false;
       }
+    }
   }
   return dp[rows][columns];
 };
