c512c5a3e7825029d0b6d37dcc10df66509dafcf
saiashish9 <saiashish3760@gmail.com>
2022-09-19 11:47:27 +0530

    k

--- dsa.js
+++ dsa.js
@@ -1509,41 +1509,60 @@
 // //   = [ [ 4, 3 ], [ 5, 3 ], [ 5, 6 ], [ 1, 2 ] ];
 // // console.log(maxEnvelopes(envelopes))
 
-var isMatch = function (s, p) {
-  const rows = s.length;
-  const columns = p.length;
-  if (rows == 0 && columns == 0) {
-    return true;
+// var isMatch = function (s, p) {
+//   const rows = s.length;
+//   const columns = p.length;
+//   if (rows == 0 && columns == 0) {
+//     return true;
+//   }
+//   if (columns == 0) {
+//     return false;
+//   }
+//   const dp = Array.from(Array(rows + 1), () => Array(columns + 1).fill(false));
+//   console.log(dp)
+//   dp[0][0] = true;
+//   for (let i = 1; i < columns + 1; i++) {
+//     if (p[i - 1] === "*") {
+//       dp[0][i] = dp[0][i - 2];
+//     } else {
+//       dp[0][i] = false;
+//     }
+//   }
+//   for (let i = 1; i < rows + 1; i++) {
+//     for (let j = 1; j < columns + 1; j++) {
+//       if (p[j - 1] === "*") {
+//         if (p[j - 2] === s[i - 1] || p[j - 2] === ".") {
+//           dp[i][j] = dp[i][j - 2] || dp[i - 1][j];
+//         } else {
+//           dp[i][j] = dp[i][j - 2];
+//         }
+//       } else if (p[j - 1] === s[i - 1] || p[j - 1] === ".") {
+//         dp[i][j] = dp[i - 1][j - 1];
+//       } else {
+//         dp[i][j] = false;
+//       }
+//     }
+//   }
+//   return dp[rows][columns];
+// };
+// console.log(isMatch("aab", "c*a*b"));
+
+var permutations = function (nums, result, visited = {}, curr = []) {
+  if ((nums.length == curr.length)) {
+    result.push(curr.slice());
+    return;
   }
-  if (columns == 0) {
-    return false;
-  }
-  const dp = Array.from(Array(rows + 1), () => Array(columns + 1).fill(false));
-  console.log(dp)
-  dp[0][0] = true;
-  for (let i = 1; i < columns + 1; i++) {
-    if (p[i - 1] === "*") {
-      dp[0][i] = dp[0][i - 2];
-    } else {
-      dp[0][i] = false;
+  for (let i = 0; i < nums.length; i++) {
+    if (!visited[i]) {
+      curr.push(nums[i]);
+      visited[i] = true;
+      permutations(nums, result, visited, curr);
+      visited[i] = false;
+      curr.pop();
     }
   }
-  for (let i = 1; i < rows + 1; i++) {
-    for (let j = 1; j < columns + 1; j++) {
-      if (p[j - 1] === "*") {
-        if (p[j - 2] === s[i - 1] || p[j - 2] === ".") {
-          dp[i][j] = dp[i][j - 2] || dp[i - 1][j];
-        } else {
-          dp[i][j] = dp[i][j - 2];
-        }
-      } else if (p[j - 1] === s[i - 1] || p[j - 1] === ".") {
-        dp[i][j] = dp[i - 1][j - 1];
-      } else {
-        dp[i][j] = false;
-      }
-    }
-  }
-  return dp[rows][columns];
 };
 
-console.log(isMatch("aab", "c*a*b"));
+const result = [];
+permutations([1, 2], result);
+console.log(result);
